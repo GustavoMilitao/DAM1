cf2176f3cc5eccbab48088e7cb940a271223ff06
Gustavo Militão <cmp.gcarmo@ifood3rd.com>
2024-04-02 20:36:06 -0300

    register screen

--- lib/login.dart
+++ lib/login.dart
@@ -2,131 +2,173 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'register.dart';
 
-class Login extends StatelessWidget {
-  const Login({super.key});
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  bool _lembrarMe = false;
+  bool _mostrarSenha = false; // Variável para controlar a visibilidade da senha
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        body: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(30.0),
-                child: Column(
-                  children: <Widget>[
-                    Container(
-                      padding: const EdgeInsets.all(5),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(10),
-                        border: Border.all(
-                            color: const Color.fromRGBO(52, 62, 255, 1)),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    padding: const EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(
+                        color: const Color.fromRGBO(52, 62, 255, 1),
                       ),
-                      child: Column(
-                        children: <Widget>[
-                          Container(
-                            padding: const EdgeInsets.all(8.0),
-                            decoration: const BoxDecoration(
-                                border: Border(
-                                    bottom: BorderSide(
-                                        color:
-                                            Color.fromRGBO(143, 148, 251, 1)))),
-                            child: TextField(
-                              decoration: InputDecoration(
-                                  border: InputBorder.none,
-                                  hintText: "Login",
-                                  hintStyle:
-                                      TextStyle(color: Colors.grey[700])),
+                    ),
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          padding: const EdgeInsets.all(8.0),
+                          decoration: const BoxDecoration(
+                            border: Border(
+                              bottom: BorderSide(
+                                color: Color.fromRGBO(143, 148, 251, 1),
+                              ),
                             ),
                           ),
-                          Container(
-                            padding: const EdgeInsets.all(8.0),
-                            child: TextField(
-                              obscureText: true,
-                              onChanged: (value) => {},
-                              decoration: InputDecoration(
-                                  border: InputBorder.none,
-                                  hintText: "Senha",
-                                  hintStyle:
-                                      TextStyle(color: Colors.grey[700])),
+                          child: TextField(
+                            keyboardType: TextInputType.emailAddress,
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "E-mail",
+                              hintStyle: TextStyle(color: Colors.grey[700]),
                             ),
-                          )
+                          ),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextField(
+                            obscureText: !_mostrarSenha,
+                            onChanged: (value) =>
+                                {}, // Adicione a lógica necessária aqui, se aplicável
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "Senha",
+                              hintStyle: TextStyle(color: Colors.grey[700]),
+                              suffixIcon: IconButton(
+                                icon: Icon(_mostrarSenha
+                                    ? Icons.visibility
+                                    : Icons.visibility_off),
+                                onPressed: () {
+                                  setState(() {
+                                    _mostrarSenha = !_mostrarSenha;
+                                  });
+                                },
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Checkbox(
+                              value: _lembrarMe,
+                              onChanged: (value) {
+                                setState(() {
+                                  _lembrarMe = !_lembrarMe;
+                                });
+                              },
+                            ),
+                            const Text('Lembrar-me')
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  SizedBox(
+                    height: 50,
+                    child: Center(
+                      child: TextButton(
+                        style: ButtonStyle(
+                          minimumSize:
+                              MaterialStateProperty.all(Size.fromHeight(50)),
+                          backgroundColor: MaterialStateProperty.all(
+                              Color.fromRGBO(143, 148, 251, 1)),
+                        ),
+                        onPressed: () {},
+                        child: const Text(
+                          "Login",
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 50),
+                  Center(
+                    child: RichText(
+                      text: TextSpan(
+                        children: [
+                          TextSpan(
+                            text: 'Esqueceu sua senha?',
+                            style: const TextStyle(
+                                color: Color.fromRGBO(143, 148, 251, 1)),
+                            recognizer: TapGestureRecognizer()..onTap = () {},
+                          ),
                         ],
                       ),
                     ),
-                    const SizedBox(height: 30),
-                    SizedBox(
-                      height: 50,
-                      child: Center(
-                        child: TextButton(
-                          style: const ButtonStyle(
-                              minimumSize:
-                                  MaterialStatePropertyAll(Size.fromHeight(50)),
-                              backgroundColor: MaterialStatePropertyAll(
-                                  Color.fromRGBO(143, 148, 251, 1))),
-                          onPressed: () {},
-                          child: const Text(
-                            "Login",
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 25),
+                  Center(
+                    child: RichText(
+                      text: TextSpan(
+                        children: [
+                          const TextSpan(
+                            text: 'Não possui uma conta? ',
+                            style: TextStyle(color: Colors.black),
                           ),
-                        ),
+                          TextSpan(
+                            text: 'Cadastre-se',
+                            style: const TextStyle(
+                                color: Color.fromRGBO(143, 148, 251, 1)),
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => const Register(),
+                                  ),
+                                );
+                              },
+                          ),
+                        ],
                       ),
                     ),
-                    const SizedBox(
-                      height: 50,
-                    ),
-                    Center(
-                      child: RichText(
-                        text: TextSpan(
-                          children: [
-                            TextSpan(
-                              text: 'Esqueceu sua senha?',
-                              style: const TextStyle(
-                                  color: Color.fromRGBO(143, 148, 251, 1)),
-                              recognizer: TapGestureRecognizer()..onTap = () {},
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 25,
-                    ),
-                    Center(
-                      child: RichText(
-                        text: TextSpan(
-                          children: [
-                            const TextSpan(
-                              text: 'Não possui uma conta? ',
-                              style: TextStyle(color: Colors.black),
-                            ),
-                            TextSpan(
-                              text: 'Cadastre-se',
-                              style: const TextStyle(
-                                  color: Color.fromRGBO(143, 148, 251, 1)),
-                              recognizer: TapGestureRecognizer()
-                                ..onTap = () {
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) =>
-                                            const Cadastro()), // Navega para a tela de cadastro
-                                  );
-                                },
-                            ),
-                          ],
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              )
-            ],
-          ),
-        ));
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/register.dart
+++ lib/register.dart
@@ -1,93 +1,235 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class Cadastro extends StatelessWidget {
-  const Cadastro({super.key});
+class Register extends StatefulWidget {
+  const Register({Key? key}) : super(key: key);
+
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  bool _mostrarSenha = false;
+  String _generoSelecionado = '';
+  bool _notificacoesEmail = false;
+  bool _notificacoesTelefone = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: const Text('Cadastro'),
-        ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(30.0),
-            child: Column(
-              children: <Widget>[
-                Container(
-                  padding: const EdgeInsets.all(5),
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(10)),
-                  child: Column(
-                    children: <Widget>[
-                      Container(
-                        padding: const EdgeInsets.all(8.0),
-                        decoration: const BoxDecoration(
-                            border: Border(
-                                bottom: BorderSide(
-                                    color: Color.fromRGBO(143, 148, 251, 1)))),
-                        child: TextField(
-                          decoration: InputDecoration(
-                              border: InputBorder.none,
-                              hintText: "Nome",
-                              hintStyle: TextStyle(color: Colors.grey[700])),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text('Cadastro'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Column(
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.all(5),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      padding: const EdgeInsets.all(8.0),
+                      decoration: const BoxDecoration(
+                        border: Border(
+                          bottom: BorderSide(
+                            color: Color.fromRGBO(143, 148, 251, 1),
+                          ),
                         ),
                       ),
-                      Container(
-                        padding: const EdgeInsets.all(8.0),
-                        decoration: const BoxDecoration(
-                            border: Border(
-                                bottom: BorderSide(
-                                    color: Color.fromRGBO(143, 148, 251, 1)))),
-                        child: TextField(
-                          decoration: InputDecoration(
-                              border: InputBorder.none,
-                              hintText: "Email",
-                              hintStyle: TextStyle(color: Colors.grey[700])),
+                      child: TextField(
+                        keyboardType: TextInputType.name,
+                        maxLength: 100, // Limite de caracteres
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          counterText: "",
+                          hintText: "Nome",
+                          hintStyle: TextStyle(color: Colors.grey[700]),
                         ),
                       ),
-                      Container(
-                        padding: const EdgeInsets.all(8.0),
-                        decoration: const BoxDecoration(
-                            border: Border(
-                                bottom: BorderSide(
-                                    color: Color.fromRGBO(143, 148, 251, 1)))),
-                        child: TextField(
-                          obscureText: true,
-                          onChanged: (value) => {},
-                          decoration: InputDecoration(
-                              border: InputBorder.none,
-                              hintText: "Senha",
-                              hintStyle: TextStyle(color: Colors.grey[700])),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(8.0),
+                      decoration: const BoxDecoration(
+                        border: Border(
+                          bottom: BorderSide(
+                            color: Color.fromRGBO(143, 148, 251, 1),
+                          ),
                         ),
-                      )
+                      ),
+                      child: TextField(
+                        keyboardType: TextInputType.datetime,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Data de Nascimento",
+                          hintStyle: TextStyle(color: Colors.grey[700]),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(8.0),
+                      decoration: const BoxDecoration(
+                        border: Border(
+                          bottom: BorderSide(
+                            color: Color.fromRGBO(143, 148, 251, 1),
+                          ),
+                        ),
+                      ),
+                      child: TextField(
+                        keyboardType: TextInputType.phone,
+                        maxLength: 11, // Limite de caracteres
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          counterText: "",
+                          hintText: "Telefone",
+                          hintStyle: TextStyle(color: Colors.grey[700]),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(8.0),
+                      decoration: const BoxDecoration(
+                        border: Border(
+                          bottom: BorderSide(
+                            color: Color.fromRGBO(143, 148, 251, 1),
+                          ),
+                        ),
+                      ),
+                      child: TextField(
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "E-mail",
+                          hintStyle: TextStyle(color: Colors.grey[700]),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(8.0),
+                      decoration: const BoxDecoration(
+                        border: Border(
+                          bottom: BorderSide(
+                            color: Color.fromRGBO(143, 148, 251, 1),
+                          ),
+                        ),
+                      ),
+                      child: TextField(
+                        obscureText: !_mostrarSenha,
+                        maxLength: 20, // Limite de caracteres
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          counterText: "",
+                          hintText: "Senha",
+                          hintStyle: TextStyle(color: Colors.grey[700]),
+                          suffixIcon: IconButton(
+                            icon: Icon(_mostrarSenha
+                                ? Icons.visibility
+                                : Icons.visibility_off),
+                            onPressed: () {
+                              setState(() {
+                                _mostrarSenha = !_mostrarSenha;
+                              });
+                            },
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 10),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Gênero',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Radio(
+                        value: 'Masculino',
+                        groupValue: _generoSelecionado,
+                        onChanged: (value) {
+                          setState(() {
+                            _generoSelecionado = value.toString();
+                          });
+                        },
+                      ),
+                      Text('Masculino'),
+                      Radio(
+                        value: 'Feminino',
+                        groupValue: _generoSelecionado,
+                        onChanged: (value) {
+                          setState(() {
+                            _generoSelecionado = value.toString();
+                          });
+                        },
+                      ),
+                      Text('Feminino'),
                     ],
                   ),
-                ),
-                const SizedBox(height: 30),
-                SizedBox(
-                  height: 50,
-                  child: Center(
-                    child: TextButton(
-                      style: ButtonStyle(
-                          minimumSize: MaterialStateProperty.all(
-                              const Size.fromHeight(50)),
-                          backgroundColor: MaterialStateProperty.all(
-                              const Color.fromRGBO(143, 148, 251, 1))),
-                      onPressed: () {},
-                      child: const Text(
-                        "Cadastrar",
-                        style: TextStyle(
-                            color: Colors.white, fontWeight: FontWeight.bold),
-                      ),
+                ],
+              ),
+              const SizedBox(height: 10),
+              Row(
+                children: [
+                  Switch(
+                    value: _notificacoesEmail,
+                    onChanged: (value) {
+                      setState(() {
+                        _notificacoesEmail = value;
+                      });
+                    },
+                  ),
+                  const Text('Ativar Notificações via E-mail'),
+                ],
+              ),
+              const SizedBox(height: 10),
+              Row(
+                children: [
+                  Switch(
+                    value: _notificacoesTelefone,
+                    onChanged: (value) {
+                      setState(() {
+                        _notificacoesTelefone = value;
+                      });
+                    },
+                  ),
+                  const Text('Ativar Notificações via Telefone'),
+                ],
+              ),
+              const SizedBox(height: 10),
+              SizedBox(
+                height: 50,
+                child: Center(
+                  child: TextButton(
+                    style: ButtonStyle(
+                      minimumSize:
+                          MaterialStateProperty.all(const Size.fromHeight(50)),
+                      backgroundColor: MaterialStateProperty.all(
+                          const Color.fromRGBO(143, 148, 251, 1)),
+                    ),
+                    onPressed: () {},
+                    child: const Text(
+                      "Cadastrar",
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
                     ),
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
